0753a77c0348640cadc2b722823af139938bd5c2
Michael Wekesa <wekesa360@outlook.com>
2022-01-27 22:55:43 +0300

    update: finalize on project

--- src/app/product-tracklisting/product-tracklisting.component.ts
+++ src/app/product-tracklisting/product-tracklisting.component.ts
@@ -1,15 +1,15 @@
 import { Component, OnInit } from '@angular/core';
 
-import { ProductService } from '../product.service';
-
 import { Album } from '../album';
 
+import { ProductService } from '../product.service';
+
 @Component({
-  selector: 'app-product-description',
-  templateUrl: './product-description.component.html',
-  styleUrls: ['./product-description.component.css']
+  selector: 'app-product-tracklisting',
+  templateUrl: './product-tracklisting.component.html',
+  styleUrls: ['./product-tracklisting.component.css']
 })
-export class ProductDescriptionComponent implements OnInit {
+export class ProductTracklistingComponent implements OnInit {
 
   albumInfo: Album;
 
